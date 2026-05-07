04b4a937e0966bf5780dbd182ed3197a9557f8ef
SerigneSaliouWADE <wadess@ept.sn>
2026-05-07 02:46:16 +0000

    [UPDATE] Styling configuration with nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,10 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className={'flex-1 items-center justify-center bg-gray-50'}>
+      <Text className={'text-2xl font-bold text-dark-200'}>
+          Movies application built with React Native
+      </Text>
     </View>
   );
 }
