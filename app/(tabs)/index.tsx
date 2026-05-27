5916b4ed94ab9b8b558869fbefe435061d2795b4
SerigneSaliouWADE <wadess@ept.sn>
2026-05-27 02:26:07 +0000

    [UPDATE] displaying movie cards data in homepage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,21 @@
-import {Image, View, ScrollView} from "react-native";
+import {ActivityIndicator, FlatList, Image, ScrollView, Text, View} from "react-native";
 import {images} from "@/constants/images";
 import {icons} from "@/constants/icons";
 import SearchBar from "@/components/SearchBar";
 import {useRouter} from "expo-router";
+import useFetch from "@/services/useFetch";
+import {fetchMovies} from "@/services/api";
+import MovieCard from "@/components/MovieCard";
 
 const HomePage = () => {
 
     const router = useRouter();
+
+    const {
+        data: movies,
+        loading: moviesLoading,
+        error: moviesError
+    } = useFetch(() => fetchMovies({searchQuery: ''}))
 
     return (
         <View className={"flex-1 bg-primary"}>
@@ -25,20 +34,59 @@ const HomePage = () => {
                     paddingBottom: 10
                 }}
             >
+
                 {/*app logo*/}
                 <Image
                     source={icons.logo}
                     className={'w-12 h-10 mt-20 mb-5 mx-auto'}
                 />
 
-                {/*    Search bar */}
-                <View className={'flex-1 mt-5'}>
-                    <SearchBar
-                        onPress={() => router.push("/search")}
-                        placeholder="Search movies, TV shows, actors..."
-                    />
-                </View>
-
+                {/*Movies loading indicator*/}
+                {
+                    moviesLoading ?
+                        (
+                            <ActivityIndicator
+                                size={'large'}
+                                color={'#0000ff'}
+                                className={'mt-10 self-center'}
+                            />
+                        )
+                        : moviesError ?
+                            (
+                                <Text> Error loading movies : {moviesError.message} </Text>
+                            )
+                            : (
+                                <View className={'flex-1 mt-5'}>
+                                    {/*Search bar */}
+                                    <SearchBar
+                                        onPress={() => router.push("/search")}
+                                        placeholder="Search movies, TV shows, actors..."
+                                    />
+                                    {/*    Movies content */}
+                                    <Text
+                                        className={'text-lg text-white font-bold mt-5 mb-3'}
+                                    >
+                                        Movies list
+                                    </Text>
+                                    <FlatList
+                                        scrollEnabled={false}
+                                        data={movies}
+                                        renderItem={({item}) => <MovieCard {...item}/>}
+                                        keyExtractor={(item) => item.id.toString()}
+                                        numColumns={3}
+                                        columnWrapperStyle={
+                                            {
+                                                justifyContent: 'flex-start',
+                                                gap: 20,
+                                                paddingRight: 5,
+                                                marginBottom: 10
+                                            }
+                                        }
+                                        className={'mt-2 pb-32'}
+                                    />
+                                </View>
+                            )
+                }
             </ScrollView>
         </View>
     )
